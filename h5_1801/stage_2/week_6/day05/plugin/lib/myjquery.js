3670fbcd0560ecd80903e7db0b5b59a5c94c9a38
方汉佳 <1690101402@qq.com>
2018-03-30 16:03:13 +0800

    添加了myjQuery插件

---
+++ h5_1801/stage_2/week_6/day05/plugin/lib/myjquery.js
@@ -0,0 +1,20 @@
+/**
+ * myjQuery插件编写：
+    *面向对象的DOM操作
+ */
+
+ // ES6的类方法
+ calss myjQuery{
+   // 初始化操作
+   constructor(selector){
+     // 传入Element或类数组Array
+     if(typeof selector != 'string'){
+       if(selector.tagName){
+         this.ele = [selector];
+       }else{
+         this.ele = selector;
+       }
+       return this;
+     }
+   }
+ }
