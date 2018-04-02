f0dfae53ad0c9a83ac827eac0c40635b8b96ec15
方汉佳 <1690101402@qq.com>
2018-04-02 10:46:11 +0800

    添加购物车飞入效果

--- h5_1801/stage_2/week_6/day06/jQuery/api/ajax_test.php
+++ h5_1801/stage_2/week_6/day06/jQuery/api/ajax_test.php
@@ -10,7 +10,30 @@
   $conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
   //设置编码
   $conn->set_charset('utf8');
+
+
+  // 数据库操作
+  // 获取所有
+  $sql = "select * from xiaomi_phone";
+  // 获取单个
+  $sql_sigle = "select * from xiaomi_phone where id='$data'";
+  // 删除商品
+  $sql_delete = "delete from xiaomi_phone where id='$data'";
   if($conn){
-    echo '连接成功'.$data;
+    if($data){
+      // 删除数据
+      $status = $conn->query($sql_delete);
+      if($status){
+        $result =$conn->query($sql);
+        $res = $result->fetch_all(MYSQLI_ASSOC);
+        echo json_encode($res,JSON_UNESCAPED_UNICODE);
+      }
+
+    }else{
+      $result =$conn->query($sql);
+      $res = $result->fetch_all(MYSQLI_ASSOC);
+      echo json_encode($res,JSON_UNESCAPED_UNICODE);
+    }
+
   }
 ?>
