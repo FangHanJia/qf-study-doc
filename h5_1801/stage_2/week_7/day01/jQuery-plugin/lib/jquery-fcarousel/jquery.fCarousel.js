3f0451af2cb78b1b17b95191c8ba90592850c244
方汉佳 <1690101402@qq.com>
2018-04-03 20:21:04 +0800

    4.3- 添加了轮播图插件和图片

--- h5_1801/stage_2/week_7/day01/jQuery-plugin/lib/jquery-fcarousel/jquery.fCarousel.js
+++ h5_1801/stage_2/week_7/day01/jQuery-plugin/lib/jquery-fcarousel/jquery.fCarousel.js
@@ -16,7 +16,7 @@
     // 使用遍历：不会覆盖实例
     return this.each(function(){
       // 这里的this指的是每一个容器
-      let $self = this;
+      let $self = $(this);
       // 对象合并
       let opt = Object.assign({},defaults,options);
 
@@ -25,7 +25,8 @@
       let $ul;
       // 上一张图的索引值
       let lastIndex = opt.index;
-
+      let $page;
+      let showAnimate;
       // 初始化方法：
       //  获取/创建元素
       //  绑定事件
@@ -49,8 +50,42 @@
           return $li;
         });
         // 添加到页面
-        $ul.append($li);
+        $ul.append($res);
         $self.append($ul);
+
+        // 创建分页
+        if(opt.page){
+          $page = $('<div/>');
+          $page.addClass('page');
+          $.map(opt.imgs,function(ele,idx){
+            // 生成相对应的页码
+            let $span = $('<span/>');
+            $span.attr('id',idx);
+            $span.text(idx+1);
+            // 初始化
+            if(idx ==0){
+              $span.addClass('active');
+            }
+            $page.append($span);
+          });
+          // 添加到页面
+          $self.append($page);
+        }
+
+        // 创建左右箭头
+        let $fprev = $('<a/>');
+        $fprev.attr('href','javasript:void(0);');
+        $fprev.addClass('fprev');
+        $fprev.addClass('arrow');
+        $fprev.text('<');
+        let $fnext = $('<a/>');
+        $fnext.attr('href','javasript:void(0);');
+        $fnext.addClass('fnext');
+        $fnext.addClass('arrow');
+        $fnext.text('>');
+        // 添加到界面
+        $self.append($fprev);
+        $self.append($fnext);
 
         // 水平滚动时必须设置ul的宽度
         if(opt.type === 'horizontal'){
@@ -59,7 +94,7 @@
         }
         // 淡入淡出必须设置定位
         else if(opt.type === 'fade'){
-          $ul.addClass = 'fade';
+          $ul.addClass('fade');
           $ul.css({
             width:opt.width,
             height:opt.height
@@ -68,46 +103,76 @@
         }
 
         // 鼠标的移入和移除
-        $self.on('mouseenter',()=>){
+        $self.on('mouseenter',()=>{
           // 清除定时器
           clearInterval($self.timer);
         }).on('mouseleave',()=>{
           move();
         })
         // 页码事件
-        .on('click','.page span',()=>{
-
+        .on('click','.page span',function(){
+          opt.index = $(this).attr('id');
+          console.log(opt.index);
+          showAnimate();
+        })
+        // 左右箭头事件
+        .on('click','.fprev',function(){
+          opt.index--;
+          showAnimate();
+        }).on('click','.fnext',function(){
+          console.log(this);
+          opt.index++;
+          showAnimate();
         });
+        move();
       }
 
       // 动画运动
       let move = function(){
-        // 重置索引值
-        if(opt.index >= opt.len){
-          opt.index = 0;
-        }else if(opt.index < 0){
-          opt.index = opt.len - 1;
+        // 封一个分页函数
+        function page(){
+          if(opt.page){
+            $page.children('span').removeClass('active');
+            $page.children('span').eq(opt.index).addClass('active');
+          }
         }
+        clearInterval($self.timer);
+        $self.timer = setInterval(function(){
+          opt.index++;
+          showAnimate();
+        },opt.duration);
+        // 封一个动画函数
+        showAnimate = function(){
+            // 重置索引值
+            if(opt.index >= opt.len){
+              opt.index = 0;
+            }else if(opt.index < 0){
+              opt.index = opt.len - 1;
+            }
 
-        // 判断运动的类型
-        let obj = {};
-        if(opt.type === 'vertical'){
-          obj.top = -opt.height * opt.index;
-          $ul.animate(obj);
-        }else if(opt.type === 'horizontal'){
-          obj.left = -opt.width * opt.index;
-          $ul.animate(obj);
-        }else if(opt.type === 'fade'){
-          // 当前图片显示
-          $ul.children('li').eq(opt.index).animate({opacity:1},function(){
-            // 将索引值复制给上一张
-            lastIndex = opt.index;
-          });
-          // 上一张隐藏
-          $ul.children('li').eq(lastIndex).animate({opacity:0},function(){
-            // 将索引值复制给上一张
-            lastIndex = opt.index;
-          });
+            // 判断运动的类型
+            let obj = {};console.log(opt.type);
+            if(opt.type === 'vertical'){
+              obj.top = -opt.height * opt.index;
+              $ul.animate(obj);
+              page();
+            }else if(opt.type === 'horizontal'){
+              obj.left = -opt.width * opt.index;
+              $ul.animate(obj);
+              page();
+            }else if(opt.type === 'fade'){
+              // 当前图片显示
+              $ul.children('li').eq(opt.index).animate({opacity:1},function(){
+                // 将索引值复制给上一张
+                lastIndex = opt.index;
+              });
+              // 上一张隐藏
+              $ul.children('li').eq(lastIndex).animate({opacity:0},function(){
+                // 将索引值复制给上一张
+                lastIndex = opt.index;
+              });
+              page();
+          }
         }
 
       }
