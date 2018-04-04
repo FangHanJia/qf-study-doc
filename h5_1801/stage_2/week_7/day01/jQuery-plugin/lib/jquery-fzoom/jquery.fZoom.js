e10a0a39ce1688e691d4d9d76d64b68dcab51420
方汉佳 <1690101402@qq.com>
2018-04-04 13:15:48 +0800

    4.4- 添加了gulp开发项目

--- h5_1801/stage_2/week_7/day01/jQuery-plugin/lib/jquery-fzoom/jquery.fZoom.js
+++ h5_1801/stage_2/week_7/day01/jQuery-plugin/lib/jquery-fzoom/jquery.fZoom.js
@@ -6,6 +6,7 @@
       height:200,
       // 放大图片的位置
       position:'right',
+      ratio:4,//比例
       // 间距
       gap:20
     }
@@ -18,27 +19,62 @@
         let $small = $(this);
         let $big;//放大图片
         let $zoom;//放大镜
-        let ratio;//比例
+        let ratio = opt.ratio;//比例
+        let $imglist;//小图片列表
 
         let zoom = {
           // 实例化
           init(){
             // 添加特定类
             $small.addClass('fzoom-small');
-            // 小图片
+
+            // 图片列表
+            $imglist = $small.next();
+            $imglist.addClass('fzoom-imglist');
             let $smallImg = $small.children('img');
-            $small.css({width:$smallImg.width()})
             // 创建大图容器
             $big = $('<div/>').addClass('fzoom-big');
+            let $bigImg ;
+            $bigImg = $('<img/>').attr('src',$smallImg.data('big'));
+            $bigImg.appendTo($big);
+
+            // 小图片点击事件
+            $imglist.on('click','li',function(){
+              let $currenturl = $(this).find('img').attr('src');
+              console.log($currenturl);
+              $smallImg.attr('src',$currenturl);
+              // 大图片
+              $bigImg = $('<img/>').attr('src',$currenturl);
+              $bigImg.appendTo($big);
+
+            });
+
+            // 小图片
+            $small.css({width:$smallImg.width()})
+            let left,top;
+            if(opt.position =='right'){
+                top  = $small.offset().top;
+                left = $small.offset().left + $smallImg.outerWidth()+opt.gap;
+            }else if(opt.position == 'bottom'){
+                left = $small.offset().left;
+                top  = $small.offset().top + $smallImg.outerHeight() + opt.gap;
+            }else if(opt.position =='left'){
+                top = $small.offset().top;
+                left = $small.offset().left - opt.gap - opt.width;
+            }else if(opt.position == 'top'){
+                left = $small.offset().left;
+                top = $small.offset().top - opt.height - opt.gap;
+            }
             // 大图片
-            let $bigImg = $('<img/>').attr('src',$smallImg.data('big'));
             // 设置样式
             $big.css({
               width:opt.width,
               height:opt.height,
+              top:top,
+              left:left
             });
             // 添加到界面
-            $bigImg.appendTo($big);
+            // $bigImg.appendTo($big);
             $big.appendTo('body');
 
             // 放大镜
@@ -48,13 +84,14 @@
             // 鼠标移入移除事件
             $small.on('mouseenter',function(){
               zoom.show();
-              if($bigImg[0].complete){
-                ratio = $bigImg.width()/$smallImg.width();
-              }else{
-                $bigImg[0].onload = function(){
+              // 有大图的情况
+                if($bigImg[0].complete){
                   ratio = $bigImg.width()/$smallImg.width();
+                }else{
+                  $bigImg[0].onload = function(){
+                    ratio = $bigImg.width()/$smallImg.width();
+                  }
                 }
-              }
 
               //设置放大镜样式
               $zoom.css({
@@ -89,6 +126,7 @@
                 top:-y*ratio
               });
             })
+
 
           },
 
